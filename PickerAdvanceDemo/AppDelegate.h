15247c65008bda05fcfcd4b89a70e1913e593fe0
pigpigdaddy <pigpigdaddy@gmail.com>
2014-08-07 20:41:21 +0800

    Advance Picker Demo
    Auto select 1 when user select 0…haha

---
+++ PickerAdvanceDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PickerAdvanceDemo
+//
+//  Created by linfeng on 14-8-7.
+//  Copyright (c) 2014年 pigpigdaddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
